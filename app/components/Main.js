e4bdb9eba96194225a9df52d8018d6571aaa8bbf
Omar Patel <omarpatel123@gmail.com>
2017-07-11 14:37:21 -0700

    add dual navigation and placeholder for image

--- app/components/Main.js
+++ app/components/Main.js
@@ -34,7 +34,7 @@ export default class Main extends Component {
                             </ul>
                             <a href="#about" className="btn btn-circle page-scroll">
                                 <i className="fa down-arrow animated">
-                                   <img className="down-arrow" src={require("../img/HomeDown.png")} alt="Down Arrow" />
+                                   <img className="down-arrow" src={require("../img/shapes/HomeDown.png")} alt="Down Arrow" />
                                 </i>
                             </a>
                         </div>
@@ -54,7 +54,7 @@ export default class Main extends Component {
         </section>
 
         <section id="user-comments" className="content-section text-center">
-            <div className="download-section">
+            <div className="user-feedback-section">
                 <div className="container">
                     <div className="col-lg-8 col-lg-offset-2">
                         <h2>TravelSafe User Comments</h2>
@@ -82,30 +82,6 @@ export default class Main extends Component {
                 </div>
             </div>
         </section>
-
-        <section id="contact" className="container content-section text-center">
-            <div className="row">
-                <div className="col-lg-8 col-lg-offset-2">
-                    <h2>Contact Start Bootstrap</h2>
-                    <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-                    <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
-                    </p>
-                    <ul className="list-inline banner-social-buttons">
-                        <li>
-                            <a href="https://twitter.com/SBootstrap" className="btn btn-default btn-lg"><i className="fa fa-twitter fa-fw"></i> <span className="network-name">Twitter</span></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/IronSummitMedia/startbootstrap" className="btn btn-default btn-lg"><i className="fa fa-github fa-fw"></i> <span className="network-name">Github</span></a>
-                        </li>
-                        <li>
-                            <a href="https://plus.google.com/+Startbootstrap/posts" className="btn btn-default btn-lg"><i className="fa fa-google-plus fa-fw"></i> <span className="network-name">Google+</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-
-        <div id="map"></div>
 
         <footer>
             <div className="container text-center">
