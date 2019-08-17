ac7af2bbfb03f1ee45dd7b74f63871f07021092b
Omar Patel <omarpatel123@yahoo.com>
2019-08-17 11:07:55 -0700

    update code to have Pricing and Nav work a bit better

--- app/components/Pricing.js
+++ app/components/Pricing.js
@@ -7,8 +7,6 @@ export default class Pricing extends Component {
   render() {
     return (
         <div>
-            <Nav />
-            <p>Yo</p>
             <Nav
             authenticated={this.props.authenticated}
             authenticate={this.props.authenticate}
@@ -16,62 +14,62 @@ export default class Pricing extends Component {
             logout={this.props.logout}
             />  
             <section id="plans">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4 text-center">
-                            <div class="panel panel-danger panel-pricing">
-                                <div class="panel-heading">
-                                    <i class="fa fa-desktop"></i>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 text-center">
+                            <div className="panel panel-danger panel-pricing">
+                                <div className="panel-heading">
+                                    <i className="fa fa-desktop"></i>
                                     <h3>Plan 1</h3>
                                 </div>
-                                <div class="panel-body text-center">
+                                <div className="panel-body text-center">
                                     <p><strong>$100 / Month</strong></p>
                                 </div>
-                                <ul class="list-group text-center">
-                                    <li class="list-group-item"><i class="fa fa-check"></i> Personal use</li>
-                                    <li class="list-group-item"><i class="fa fa-check"></i> Unlimited projects</li>
-                                    <li class="list-group-item"><i class="fa fa-check"></i> 27/7 support</li>
+                                <ul className="list-group text-center">
+                                    <li className="list-group-item"><i className="fa fa-check"></i> Personal use</li>
+                                    <li className="list-group-item"><i className="fa fa-check"></i> Unlimited projects</li>
+                                    <li className="list-group-item"><i className="fa fa-check"></i> 27/7 support</li>
                                 </ul>
-                                <div class="panel-footer">
-                                    <a class="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
+                                <div className="panel-footer">
+                                    <a className="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 text-center">
-                            <div class="panel panel-warning panel-pricing">
-                                <div class="panel-heading">
-                                    <i class="fa fa-desktop"></i>
+                        <div className="col-md-4 text-center">
+                            <div className="panel panel-warning panel-pricing">
+                                <div className="panel-heading">
+                                    <i className="fa fa-desktop"></i>
                                     <h3>Plan 2</h3>
                                 </div>
-                                <div class="panel-body text-center">
+                                <div className="panel-body text-center">
                                     <p><strong>$200 / Month</strong></p>
                                 </div>
-                                <ul class="list-group text-center">
-                                    <li class="list-group-item"><i class="fa fa-check"></i> Personal use</li>
-                                    <li class="list-group-item"><i class="fa fa-check"></i> Unlimited projects</li>
-                                    <li class="list-group-item"><i class="fa fa-check"></i> 27/7 support</li>
+                                <ul className="list-group text-center">
+                                    <li className="list-group-item"><i className="fa fa-check"></i> Personal use</li>
+                                    <li className="list-group-item"><i className="fa fa-check"></i> Unlimited projects</li>
+                                    <li className="list-group-item"><i className="fa fa-check"></i> 27/7 support</li>
                                 </ul>
-                                <div class="panel-footer">
-                                    <a class="btn btn-lg btn-block btn-warning" href="#">BUY NOW!</a>
+                                <div className="panel-footer">
+                                    <a className="btn btn-lg btn-block btn-warning" href="#">BUY NOW!</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 text-center">
-                            <div class="panel panel-success panel-pricing">
-                                <div class="panel-heading">
-                                    <i class="fa fa-desktop"></i>
+                        <div className="col-md-4 text-center">
+                            <div className="panel panel-success panel-pricing">
+                                <div className="panel-heading">
+                                    <i className="fa fa-desktop"></i>
                                     <h3>Plan 3</h3>
                                 </div>
-                                <div class="panel-body text-center">
+                                <div className="panel-body text-center">
                                     <p><strong>$300 / Month</strong></p>
                                 </div>
-                                <ul class="list-group text-center">
-                                    <li class="list-group-item"><i class="fa fa-check"></i> Personal use</li>
-                                    <li class="list-group-item"><i class="fa fa-check"></i> Unlimited projects</li>
-                                    <li class="list-group-item"><i class="fa fa-check"></i> 27/7 support</li>
+                                <ul className="list-group text-center">
+                                    <li className="list-group-item"><i className="fa fa-check"></i> Personal use</li>
+                                    <li className="list-group-item"><i className="fa fa-check"></i> Unlimited projects</li>
+                                    <li className="list-group-item"><i className="fa fa-check"></i> 27/7 support</li>
                                 </ul>
-                                <div class="panel-footer">
-                                    <a class="btn btn-lg btn-block btn-success" href="#">BUY NOW!</a>
+                                <div className="panel-footer">
+                                    <a className="btn btn-lg btn-block btn-success" href="#">BUY NOW!</a>
                                 </div>
                             </div>
                         </div>
--- app/components/Trips/Trip.js
+++ app/components/Trips/Trip.js
@@ -6,36 +6,40 @@ import Login from '../Login';
 import NewTrip from '../children/NewTrips/NewTrip';
 
 export default class Trip extends Component {
-<div class="all-trips-container container">
 	
-	<div class="row">
-		{{#if trip }}
-		{{#each trip}}
-		<div class="trip-container col-xs-12 col-sm-6 col-lg-6">
-			<div class="trip-subcontainer trip-default">
-				<h3 class="lead">
-					Trip: {{ this.name }} Test trip name
-				</h3>
-				<div class="bottom-trip-container">
-					<div class="risk-analysis-container">
-							{{!-- {{ riskAnalysis }} --}}
-						<p class="risk-analysis">
-							Some Kind of Risk Analysis
-						</p>
+render() {
+	return (
+		<div className="all-trips-container container">
+			<div className="row">
+				{{#if trip }}
+				{{#each trip}}
+				<div className="trip-container col-xs-12 col-sm-6 col-lg-6">
+					<div className="trip-subcontainer trip-default">
+						<h3 className="lead">
+							Trip: {{ this.name }} Test trip name
+						</h3>
+						<div className="bottom-trip-container">
+							<div className="risk-analysis-container">
+									{{!-- {{ riskAnalysis }} --}}
+								<p className="risk-analysis">
+									Some Kind of Risk Analysis
+								</p>
+							</div>
+							<div className="destination-container">
+									{{!-- {{ riskAnalysis }} --}}
+								<p className="destination">
+									{{ this.destination }}
+								</p>
+							</div>
+							<button type="button" className="btn btn-circle"><i className="">10</i></button>
+						</div>
 					</div>
-					<div class="destination-container">
-							{{!-- {{ riskAnalysis }} --}}
-						<p class="destination">
-							{{ this.destination }}
-						</p>
-					</div>
-					<button type="button" class="btn btn-circle"><i class="">10</i></button>
 				</div>
+				{{/each}}
+				{{/if}}
+			<button id="add-trip-button" data-toggle="modal" data-target="#squarespaceModal" type="button" class="btn btn-circle"><i class="glyphicon glyphicon-plus"></i></button>
 			</div>
 		</div>
-		{{/each}}
-		{{/if}}
-	<button id="add-trip-button" data-toggle="modal" data-target="#squarespaceModal" type="button" class="btn btn-circle"><i class="glyphicon glyphicon-plus"></i></button>
-	</div>
-</div>
+	)
+}
 }
